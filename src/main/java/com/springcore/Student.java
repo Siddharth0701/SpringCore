17e86377f2adf320dec6d027aec59d153b370668
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-12-17 16:19:35 +0530

    initial commit

---
+++ src/main/java/com/springcore/Student.java
@@ -0,0 +1,46 @@
+package com.springcore;
+
+public class Student {
+		private int studentId;
+		private String studentName;
+		private String studentCity;
+		
+		public int getStudentId() {
+			return studentId;
+		}
+		public void setStudentId(int studentId) {
+			System.out.println("Setting student id");
+			this.studentId = studentId;
+		}
+		public String getStudentName() {
+			return studentName;
+		}
+		public void setStudentName(String studentName) {
+			System.out.println("Setting student name");
+			this.studentName = studentName;
+		}
+		public String getStudentCity() {
+			return studentCity;
+		}
+		public void setStudentCity(String studentCity) {
+			System.out.println("Setting student city");
+			this.studentCity = studentCity;
+		}
+
+		public Student(int studentId, String studentName, String studentCity) {
+			super();
+			this.studentId = studentId;
+			this.studentName = studentName;
+			this.studentCity = studentCity;
+		}
+		
+		public Student() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		@Override
+		public String toString() {
+			return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentCity=" + studentCity
+					+ "]";
+		}
+}
